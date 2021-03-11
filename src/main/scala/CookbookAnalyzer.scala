9a4afb5e9d9ea9e7b2d2c261292bf6ec2f0914b9
Dagnija-K <dagnijakaleja@gmail.com>
2021-03-11 14:51:46 +0200

    final CookbookAnalyzer

--- src/main/scala/CookbookAnalyzer.scala
+++ src/main/scala/CookbookAnalyzer.scala
@@ -7,30 +7,7 @@ object CookbookAnalyzer extends App {
 
   val relative_path = "src/resources/cookbook.txt"
 
-//  def getLinesFromFile(srcPath: String) = {
-//    val bufferedSource = Source.fromFile(srcPath)
-//    val lines = bufferedSource.getLines.toSeq
-//    bufferedSource.close
-//    lines
-//  }
-
   val lines = Utilities.getLinesFromFile(relative_path)
-
-
-//  def getRecipeTitles(text: Array[String]): Array[String] = {
-//    val recipeTitles = slicedText.filter(line => line.nonEmpty && line.toUpperCase == line && !line.startsWith("   "))
-//    recipeTitles
-//  }
-////
-////  val recipeTitles = getRecipeTitles(slicedText)
-////  recipeTitles.foreach(println)
-////
-//  def getIngredients(text: Array[String]): Array[String] = {
-//    slicedText.filter(line => line.startsWith("   ") && line.nonEmpty)
-//  }
-
-//  val ingredients = getIngredients(slicedText)
-//
 
   def findNeedle(lines: Seq[String], needle: String): Int = {
     lines.indexWhere(line => line.toLowerCase.contains(needle.toLowerCase))
@@ -59,11 +36,6 @@ object CookbookAnalyzer extends App {
     initialResult
     }
 
-
-//  def getRecipeAndTitles(lines: Map[String, Seq[String]]) : Map[String, Seq[String]] = {
-//    for { (k, v) <- recipeAnditlesMap if v.contains(value) } yield k
-//  }
-
   val relative_save_path = "src/resources/cookbook_cleaned.txt"
 
   import java.io.{PrintWriter, File}
@@ -73,104 +45,13 @@ object CookbookAnalyzer extends App {
 
 //  for ((key, value) <- finalRes) {
   for (key <- recipeTitles; value <- finalRes1 get key) {
-
-   if(!value.isEmpty){
-
-
-   // println(s"$key")
-//    value.foreach(println)
+   if(!value.isEmpty) {
     pw.write(s"$key "+"\n")
-
     value.foreach{(ingredient:String) => pw.write(ingredient +"\n" )}
-
     pw.write("\n")
-   }
-//    for (key <- value.ingredients ; value2 <- finalRes1 get key) {}
-   // value.foreach(ingredients => ingredients.split("\\n+"))
     }
+   }
 
-
-//  pw.write(saveTxt)
   pw.close()
 
-//val finalRes2 = finalRes1 foreach (x => println (x._1 + " - " + x._2))
-//
-//  val finalRes
-
-//  val finished = finalRes.filter(_._2.contains(value)).map(_._1)
-
-//  for { (k, v) <- finalRes if v.contains(value)} yield k
-//
-//      println(s"$key")
-//      value.foreach(ingredients => ingredients.split("\\n+").foreach(println))
-//
-//  finalRes.foreach(println)
-//    for ((key, value) <- finalRes)
-//  for { (k, v) <- finalRes if v.contains(value) } yield k
-//  {
-
-//  finalRes.foreach(println)
-
-// val finalRes =  res1.flatMap{ case (key,value) => if (value.contains(s"$ingredients")) Seq(key,value) else Seq() }
-//  println(finalRes)
-
-//  val finalRes = getResult(slicedLines, recipeTitles)
-//     for ((key, value) <- finalRes) {
-//       println(s"$key")
-//       value.foreach(println)
-//       value.foreach(ingredients => ingredients.split(" \\s+"))
-//     }
-
-//    for (key <- recipeTitles; value <- finalRes get key) yield (key,value) {
-//      println(s"$key")
-//      value.foreach(println)
-//      value.foreach(ingredients => ingredients.split(" \\s+"))
-//    }
-
-////  finalRes.foreach(println)
-
-//  recipeTitles collect finalRes
-  //  for (key <- recipeTitles; value <- finalRes get key)
-  //  val filteredResult = finalRes.filter
-
-
-  //  val rec = finalRes.filter(_._2.contains(ingredients)).map(_._1)
-  //  result.foreach(println)
-
-//
-//  val resultSeq = getResult(slicedLines, recipeTitles)
-//  for ((key, value) <- resultSeq) {
-//   value.foreach(ingredients => ingredients.split("\\n+"))
-//  }
-
-
-//    def getRelevantText(slicedLines: Array[String]) : Boolean = {
-//      getRecipeTitles(slicedLines) || getIngredients(slicedLines)
-//    }
-//
-//  val relevantText = slicedLines.filter(getRecipeTitles && getIngredients).map
-
-
-
-//  val relevantLines = slicedLines.filter(line => (line == line.toUpperCase) || (line => line.startsWith("    ")))
-//val filteredRecipes = slicedLines.filter((line => line.length > 0 && line)
-
-
-
-
-//  val recipeTitlesMap = recipeTitles.map(recipe => (recipeTitles, ArrayBuffer[String]())).toMap
-
-//  val saveTxt = result
-
-//   val saveTxt = finalRes1.mkString("\n")
-//  saveTxt.foreach(println)
-
-//  val relative_save_path = "src/resources/cookbook_cleaned.txt"
-//
-//   import java.io.{PrintWriter, File}
-//    val pw = new PrintWriter(new File(relative_save_path))
-//    pw.write(saveTxt)
-//    pw.close()
-
-//  Utilities.saveLines(saveTxt, destPath = relative_save_path)
 }
